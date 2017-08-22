3d9cf47eb4d5040d733cd462025aaa168dab85d4
rkwalters <rwalters@broadinstitute.org>
2017-08-22 10:50:20 -0400

    add partitioned h2

---
+++ ldsc_h2part_parallel_batch_submit.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+set -e
+
+# hard-parallelize ukbb ldsc jobs
+# - loops specified number of batches
+# - spins up cluster
+# - submits ldsc batch in background
+
+maxi=$((8))
+
+for i in `seq 2 $maxi`; do
+# for i in `seq 1 1`; do
+	
+	cluster start ukbb-rkw${i} -m n1-standard-16 --num-workers 0 --num-preemptible-workers 0
+	cluster submit ukbb-rkw${i} ldsc_h2part_parallel_batch.py --args "--parsplit ${maxi} --paridx ${i}" &
+
+done
+
+
+# eof
