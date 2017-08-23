47d94a9361207a2ec1da9febda23c4256b465051
rkwalters <rwalters@broadinstitute.org>
2017-08-22 20:17:42 -0400

    fix R parsing issue

--- make_rdata.R
+++ make_rdata.R
@@ -3,7 +3,7 @@
 infile <- commandArgs(T)[1]
 outname <- commandArgs(T)[2]
 
-dat <- read.table(infile, header=T, stringsAsFactors=F, sep='\t')
+dat <- read.table(infile, header=T, stringsAsFactors=F, sep='\t', quote="")
 save(dat, file=outname)
 
 summary(dat)
