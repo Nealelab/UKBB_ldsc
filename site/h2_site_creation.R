dbe2b47a8ae8cbbcab525819ca1b4b9768adbbbb
rkwalters <rwalters@broadinstitute.org>
2017-09-14 03:39:31 -0400

    condense separate pages to site

--- site/h2_site_creation.R
+++ site/h2_site_creation.R
@@ -1,36 +1,44 @@
 ###
 # run rmarkdown reports for each phenotype
 ###
-setwd("/Users/raymondwalters/Documents/Code/github/UKBB_ldsc/")
+setwd("/Users/raymondwalters/Documents/Code/github/UKBB_ldsc/site/")
 
 # load packages
 rm(list=ls())
+# devtools::install_github("rstudio/rmarkdown",ref="bcdb1a28b231576a98dea398986a868da804e609")
 require("rmarkdown")
 
+# build
+render_site()
+
+
+
+#####################
+
+### old
+
 # load univar data
-load("./results/ukbb_h2univar.RData")
-isUnivar <- T
+# load("./results/ukbb_h2univar.RData")
+# isUnivar <- T
 
 # univar browser
 # render('./site/h2_browser.Rmd',output_file='h2_univar_browser.html',output_dir = './docs')
 
 # load partitioned data
-load("./results/ukbb_h2part.RData")
-isUnivar <- F
+# load("./results/ukbb_h2part.RData")
+# isUnivar <- F
 
 # partitioned browser
 # render('./site/h2_browser.Rmd',output_file='h2_browser.html',output_dir = './docs')
 
 # front pages
-render('./site/welcome.Rmd',output_file='index.html',output_dir = './docs')
-render('./site/details.Rmd',output_file='details.html',output_dir = './docs')
+# render('./site/welcome.Rmd',output_file='index.html',output_dir = './docs')
+# render('./site/details.Rmd',output_file='details.html',output_dir = './docs')
 
 # loop pheno pages
 # for(phen in dat$phenotype[c(1:5,sample(1:nrow(dat),5))]){
+#	dat_sub <- dat[which(dat$phenotype==phen),]
 #	render('./site/h2_part_template.Rmd',
 #		   output_file=paste0('h2_summary_',phen,'.html'),
 #		   output_dir='./docs')
-# }
-
-
-
+#}
